084b393d026615cc0a6fab5c29efcb43e064bbf2
NereusP <fangyuping0521@gmail.com>
2018-12-17 22:42:38 +0800

    Update sht2x.hpp

--- sht2x/sht2x.hpp
+++ sht2x/sht2x.hpp
@@ -1,3 +1,12 @@
+/*
+版权声明：
+本代码未经本人许可不得用于商业用途，一经发现，按一人民币一个字符（包含空格和标点）收取费用。一旦你使用了本代码，默认你已经阅读并接受该声明
+
+Copyright Notice：
+This code may not be used for commercial purposes without my permission. Once found, it is charged at a rate of ￥1
+(including spaces and punctuation). Once you have used this code, by default you have read and accepted the statement.
+*/
+
 #ifndef _SHT2X_H_
 #define _SHT2X_H_
 
