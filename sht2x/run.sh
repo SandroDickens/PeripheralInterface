c460e4640fc9a8fd3444afab008487a07c6efa06
anonymous <anonymous@samchiang.com>
2021-09-07 00:18:08 +0800

    update CMakeLists.txt
    update CMakeLists.txt

---
+++ sht2x/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+rm -rf build
+cmake -S . -B ./build -DCMAKE_BUILD_TYPE="Release"
+cd ./build
+make
+cd ..
--- sht2x/src/main.cpp
+++ sht2x/src/main.cpp
@@ -9,13 +9,13 @@ int main()
 	SHT2x *sht20 = SHT2x::getInstance("/dev/i2c-0", I2C_DEV_ADDR);
 	if (sht20 != nullptr)
 	{
-		for (int i = 0; i < 30; i++)
-		{
-			float temp = sht20->getTemperature();
-			float humi = sht20->getHumidity();
-			printf("T:%2.3f, H:%2.4f\n", temp, humi);
-			sleep(1);
-		}
+		float temp = sht20->getTemperature();
+		float humi = sht20->getHumidity();
+		printf("%2.2f, %2.2f\n", temp, humi);
+		return 0;
 	}
-	return 0;
+	else
+	{
+		return -1;
+	}
 }
